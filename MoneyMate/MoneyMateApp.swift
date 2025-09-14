f3da924c627c81c634fdb52b645da0cb5d522fee
Karan Kumar <thekarankumar.dev@gmail.com>
2025-09-14 12:20:27 +0530

    Initial Commit

---
+++ MoneyMate/MoneyMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMateApp.swift
+//  MoneyMate
+//
+//  Created by Karan Kumar on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
