2d0bb70e0d570681575df1810142b246ec1bb772
Karan Kumar <thekarankumar.dev@gmail.com>
2025-09-17 15:57:29 +0530

    fixed-onboard

--- MoneyMate/OnboardingView.swift
+++ MoneyMate/OnboardingView.swift
@@ -12,45 +12,51 @@ struct OnboardingView: View {
     let images = ["First", "Second", "Third"]
     let titles = ["Track Your Expenses", "Set Goals", "Analyze Your Spending"]
     let subtitles = ["Know where your money goes", "Plan better for the future", "Make informed decisions"]
-
+    
     
     let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        VStack {
-            Spacer().frame(maxHeight: 30)
-            
-            TabView(selection: $currentPage){
-                ForEach(0..<titles.count, id:\.self){ index in
-                    
-                    VStack(spacing: 20) {
-                        Image(images[index])
-                            .resizable()
-                            .scaledToFit() // keeps aspect ratio, no stretch
-                            .frame(maxHeight: 300)
-                            .foregroundColor(.blue)
-                            .transition(.slide) // slide transition
-                            .animation(.easeInOut, value: currentPage)
+        
+        ZStack{
+            Color(red: 246/255, green: 246/255, blue: 246/255).ignoresSafeArea()
+            VStack {
+               
+                Spacer().frame(maxHeight: 30)
+                
+                TabView(selection: $currentPage){
+                    ForEach(0..<titles.count, id:\.self){ index in
                         
-                        Text(titles[index])
-                            .font(.title)
-                            .bold()
-                            .transition(.opacity.combined(with: .scale))
-                            .animation(.easeInOut, value: currentPage)
+                        VStack(spacing: 20) {
+                            Image(images[index])
+                                .resizable()
+                                .scaledToFit() // keeps aspect ratio, no stretch
+                                .frame(maxHeight: 300)
+                                .foregroundColor(.blue)
+                                .transition(.slide) // slide transition
+                                .animation(.easeInOut, value: currentPage)
+                            
+                            Text(titles[index])
+                                .font(.title)
+                                .bold()
+                                .transition(.opacity.combined(with: .scale))
+                                .animation(.easeInOut, value: currentPage)
+                            
+                            Text(subtitles[index])
+                                .font(.subheadline)
+                                .multilineTextAlignment(.center)
+                                .padding(.horizontal, 30)
+                                .transition(.opacity)
+                                .animation(.easeInOut, value: currentPage)
+                        }
+                        .tag(index)
+                        .background(Color(red: 246/255, green: 246/255, blue: 246/255))
                         
-                        Text(subtitles[index])
-                            .font(.subheadline)
-                            .multilineTextAlignment(.center)
-                            .padding(.horizontal, 30)
-                            .transition(.opacity)
-                            .animation(.easeInOut, value: currentPage)
-                                }
-                                .tag(index)
-
+                    }
                 }
-            }
-            .frame(maxHeight: 500)
-            .tabViewStyle(PageTabViewStyle())
+                .frame(maxHeight: 500)
+                
+                .tabViewStyle(PageTabViewStyle())
                 .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
                 .onReceive(timer){ _ in
                     withAnimation{
@@ -58,53 +64,55 @@ struct OnboardingView: View {
                     }
                     
                 }
-            
-          
-            Spacer().frame(maxHeight: 30)
-            
-            
-            
-            // Apple Sign In Button
-            SignInWithAppleButton(.signIn){ request in
-                request.requestedScopes = [.fullName, .email]
                 
-            } onCompletion: { result in
-                print(result)
-            }.signInWithAppleButtonStyle(.black)
-                .frame(height: 50)
-                .frame(maxWidth: .infinity) // full width
+                
+                Spacer().frame(maxHeight: 30)
+                
+                
+                
+                // Apple Sign In Button
+                SignInWithAppleButton(.signIn){ request in
+                    request.requestedScopes = [.fullName, .email]
+                    
+                } onCompletion: { result in
+                    print(result)
+                }.signInWithAppleButtonStyle(.black)
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity) // full width
+                    .padding(.horizontal, 20)
+                
+                
+                
+                // 🔹 Google Sign In
+                Button {
+                    // handle Google sign in
+                    print("Google tapped")
+                } label: {
+                    HStack {
+                        Image("googleLogo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .foregroundColor(.red)
+                            .font(.title2)
+                        Text("Sign in with Google")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 22) // full width
+                    .padding()
+                    .background(Color.white)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10, style: .continuous).stroke(Color.gray, lineWidth: 1)
+                    )
+                    .cornerRadius(10)
+                    
+                }
                 .padding(.horizontal, 20)
-            
-            
-            
-            // 🔹 Google Sign In
-                        Button {
-                            // handle Google sign in
-                            print("Google tapped")
-                        } label: {
-                            HStack {
-                                Image("googleLogo")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 30, height: 30, alignment: .center)
-                                    .foregroundColor(.red)
-                                    .font(.title2)
-                                Text("Sign in with Google")
-                                    .font(.headline)
-                                    .foregroundColor(.black)
-                            }
-                            .frame(maxWidth: .infinity, maxHeight: 25) // full width
-                            .padding()
-                            .background(Color.white)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10, style: .continuous).stroke(Color.gray, lineWidth: 1)
-                            )
-                            .cornerRadius(10)
-                            
-                        }
-                        .padding(.horizontal, 20)
+            }
+           
         }
-        .background(Color(red: 246/255, green: 246/255, blue: 246/255))
+      
         
     }
 }
